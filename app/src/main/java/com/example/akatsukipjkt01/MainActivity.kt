7d440e72f29bf5cac7a89f1f107de6ab4ba89b2e
boyinrealblues <therealraghavkashyap26@gmail.com>
2021-03-09 17:55:09 +0530

    this project now has Data Binding Enabled

--- app/src/main/java/com/example/akatsukipjkt01/MainActivity.kt
+++ app/src/main/java/com/example/akatsukipjkt01/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.akatsukipjkt01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.akatsukipjkt01.databinding.ActivityMainBinding
 
+lateinit private var binding:ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
