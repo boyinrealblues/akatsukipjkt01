b9cf0931349c483fe5ddcf6d8eaf262b4d0f6006
boyinrealblues <therealraghavkashyap26@gmail.com>
2021-03-10 14:22:21 +0530

    editable text w data updation via dataBinding; bug found

--- app/src/main/java/com/example/akatsukipjkt01/MainActivity.kt
+++ app/src/main/java/com/example/akatsukipjkt01/MainActivity.kt
@@ -2,15 +2,25 @@ package com.example.akatsukipjkt01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.akatsukipjkt01.databinding.ActivityMainBinding
+import java.util.logging.Logger
 
 lateinit private var binding:ActivityMainBinding
-data class User(val firstName:String,val lastName:String)
+data class User(var firstName:String)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
-        binding.name=User("Raghav","Kashyap")
+        changeName()
+    }
+    fun changeName(){
+        binding.button.setOnClickListener {
+            binding.editName.text=binding.editName.editableText
+            Log.e(binding.editName.editableText.toString(),"MainActivity")
+            binding.text=User(binding.editName.text.toString())
+        }
     }
 }
